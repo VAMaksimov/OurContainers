dbfc5f03a5fa45995c1aa8cc0435092032774e80
Contesz <countesz@student.21-school.ru>
2025-02-23 20:05:36 +0700

    init and test for list

---
+++ src/components/s21_list.h
@@ -0,0 +1,276 @@
+#ifndef S21_CONTAINERS_H_S21_LIST_H
+#define S21_CONTAINERS_H_S21_LIST_H
+
+#include "components/s21_sequence_containers.h"
+
+namespace s21 {
+template <typename T>
+class list : public SequenceContaner<list<T>, T> {
+ public:
+  //  List Member type
+  using value_type = typename SequenceContaner<list<T>, T>::value_type;
+  using reference = typename SequenceContaner<list<T>, T>::reference;
+  using const_reference =
+      typename SequenceContaner<list<T>, T>::const_reference;
+  using size_type = typename SequenceContaner<list<T>, T>::size_type;
+
+  struct Node_ {
+    value_type value;
+    Node_* next;
+    Node_* prev;
+
+    // Constructor fake Node
+    explicit Node_(Node_* next_node = nullptr, Node_* prev_node = nullptr)
+        : value(), next(next_node), prev(prev_node) {}
+    // Constructor real Node
+    explicit Node_(value_type val, Node_* next_node = nullptr,
+                   Node_* prev_node = nullptr)
+        : value(val), next(next_node), prev(prev_node) {}
+  };
+  // Сonstructors
+  list() noexcept;
+  explicit list(size_type n);
+  list(std::initializer_list<value_type> const& items);
+  list(const list& other) noexcept;
+  list(list&& other) noexcept;
+  // Destructor
+  ~list();
+
+  // Operators
+
+  list<T>& operator=(list<T>&& other) noexcept;
+  list<T>& operator=(const list<T>& other) noexcept;
+
+  // Mutators and accesors
+  size_type getSize() const { return size; }
+  Node_* getHead() const { return fake->next; }
+  Node_* getTail() const { return fake->prev; }
+
+  // Iterators
+  class ListIterator {
+   public:
+    ListIterator() = default;
+    ListIterator(Node_* node);
+    ~ListIterator() = default;
+
+    ListIterator& operator++();
+
+    ListIterator operator++(int);
+
+    ListIterator& operator--();
+
+    ListIterator operator--(int);
+    reference operator*() const;
+
+    bool operator==(const ListIterator& other) const;
+
+    bool operator!=(const ListIterator& other) const;
+
+   private:
+    Node_* current;
+  };
+
+  using iterator = ListIterator;
+  //   using const_iterator = ListConstIterator;
+
+  iterator begin() const noexcept;
+  iterator end() const noexcept;
+
+  // Methoods
+  void clear();
+  void swap(list& other) noexcept;
+
+ private:
+  Node_* fake;
+  size_type size;
+  void initFakeNode();
+};
+
+template <typename T>
+inline void list<T>::initFakeNode() {
+  fake = new Node_();
+  fake->next = fake;
+  fake->prev = fake;
+}
+
+// Constructors default
+template <typename T>
+inline list<T>::list() noexcept : size(0) {
+  initFakeNode();
+}
+
+// Constructors param
+template <typename T>
+inline list<T>::list(size_type n) : size(0) {
+  initFakeNode();
+  for (size_type i = 0; i < n; ++i) {
+    Node_* newNode = new Node_(value_type());
+    if (fake->next == fake) {
+      fake->next = fake->prev = newNode;
+    } else {
+      newNode->prev = fake->prev;
+      fake->prev->next = newNode;
+    }
+    fake->prev = newNode;
+    newNode->next = fake;
+    ++size;
+  }
+}
+
+// Constructor {}
+template <typename T>
+inline list<T>::list(std::initializer_list<value_type> const& items) : size(0) {
+  initFakeNode();
+  for (const auto& item : items) {
+    Node_* newNode = new Node_(item);
+    newNode->next = fake;
+    newNode->prev = fake->prev;
+    fake->prev->next = newNode;
+    fake->prev = newNode;
+    ++size;
+  }
+}
+
+// constructor move
+template <typename T>
+inline list<T>::list(list&& other) noexcept {
+  if (other.fake != nullptr) {
+    fake = other.fake;
+    size = other.size;
+
+    other.fake = nullptr;
+    other.size = 0;
+    other.initFakeNode();
+  };
+}
+
+// Constructor copy
+template <typename T>
+inline list<T>::list(const list& other) noexcept : size(other.size) {
+  initFakeNode();
+  Node_* currentNode = other.getHead();
+  while (currentNode != other.fake) {
+    Node_* newNode = new Node_(currentNode->value);
+    newNode->prev = fake->prev;
+    fake->prev->next = newNode;
+    fake->prev = newNode;
+    newNode->next = fake;
+    currentNode = currentNode->next;
+  }
+}
+
+// Destructor
+template <typename T>
+inline list<T>::~list() {
+  clear();
+  delete[] fake;
+}
+
+template <typename T>
+inline void list<T>::clear() {
+  while (fake->next != fake) {
+    Node_* temp = fake->next;
+    fake->next = temp->next;
+    delete[] temp;
+  }
+  fake->prev = fake;
+  size = 0;
+}
+
+/*Operators*/
+
+template <typename T>
+inline list<T>& list<T>::operator=(list<T>&& other) noexcept {
+  if (this != &other) {
+    // Очищаем текущий объект
+    clear();
+    delete[] fake;
+
+    if (other.fake != nullptr) {
+      // Перемещаем данные из other в текущий объект
+      fake = other.fake;
+      size = other.size;
+
+      // Обнуляем other
+      other.fake = nullptr;
+      other.size = 0;
+      other.initFakeNode();
+    }
+  }
+  return *this;
+}
+
+template <typename T>
+inline list<T>& list<T>::operator=(const list<T>& other) noexcept {
+  if (this != &other) {
+    list<T> temp(other);
+    swap(temp);
+  }
+  return *this;
+}
+
+/* methods ListIterator*/
+
+template <typename T>
+inline list<T>::ListIterator::ListIterator(Node_* node) : current(node) {}
+
+template <typename T>
+inline typename list<T>::ListIterator& list<T>::ListIterator::operator++() {
+  current = current->next;
+  return *this;
+}
+
+template <typename T>
+inline typename list<T>::ListIterator list<T>::ListIterator::operator++(int) {
+  ListIterator temp = *this;
+  ++(*this);
+  return temp;
+}
+
+template <typename T>
+inline typename list<T>::ListIterator& list<T>::ListIterator::operator--() {
+  current = current->prev;
+  return *this;
+}
+
+template <typename T>
+inline typename list<T>::ListIterator list<T>::ListIterator::operator--(int) {
+  ListIterator temp = *this;
+  --(*this);
+  return temp;
+}
+
+template <typename T>
+inline bool list<T>::ListIterator::operator==(const ListIterator& other) const {
+  return current == other.current;
+}
+
+template <typename T>
+inline bool list<T>::ListIterator::operator!=(const ListIterator& other) const {
+  return current != other.current;
+}
+
+template <typename T>
+inline typename list<T>::iterator list<T>::begin() const noexcept {
+  return iterator(fake->next);
+}
+
+template <typename T>
+inline typename list<T>::iterator list<T>::end() const noexcept {
+  return iterator(fake);
+}
+
+template <typename T>
+inline typename list<T>::reference list<T>::ListIterator::operator*() const {
+  return current->value;
+}
+
+/*Capacity*/
+template <typename T>
+void list<T>::swap(list& other) noexcept {
+  std::swap(this->fake, other.fake);
+  std::swap(this->size, other.size);
+}
+
+}  // namespace s21
+#endif  // S21_CONTAINERS_H_S21_LIST_H
---
+++ src/tests/s21_list_test.cc
@@ -0,0 +1,162 @@
+#include "components/s21_list.h"
+
+#include <gtest/gtest.h>
+
+#include "s21_containers.h"
+TEST(Test_List, Default_Constructor) {
+  s21::list<int> myList;
+  EXPECT_EQ(myList.getSize(), 0);
+  EXPECT_EQ(myList.getHead(),
+            myList.getTail());  // ptr fake node: ptr head = ptr tail
+}
+
+TEST(Test_List, Parameterized_Constructor) {
+  s21::list<int> myList(1);
+  EXPECT_EQ(myList.getHead(), myList.getTail());
+  s21::list<int> myList2(5);
+  EXPECT_EQ(myList2.getSize(), 5);
+  EXPECT_NE(myList2.getHead(), myList2.getTail());
+}
+
+TEST(Test_List, Iterator_Test) {
+  s21::list<int> myList = {1, 2, 3, 4, 5};
+  s21::list<int>::iterator it = myList.begin();
+
+  EXPECT_EQ(*it, 1);
+  it++;
+  EXPECT_EQ(*it, 2);
+  ++it;
+  EXPECT_EQ(*it, 3);
+  ++it;
+  EXPECT_EQ(*it, 4);
+  ++it;
+  EXPECT_EQ(*it, 5);
+  ++it;
+  EXPECT_EQ(it, myList.end());
+}
+
+TEST(Test_List, Iterator_Decrement_Test) {
+  s21::list<int> myList = {1, 2, 3, 4, 5};
+  s21::list<int>::iterator it = myList.end();
+  --it;
+  EXPECT_EQ(*it, 5);
+  --it;
+  EXPECT_EQ(*it, 4);
+  --it;
+  EXPECT_EQ(*it, 3);
+  --it;
+  EXPECT_EQ(*it, 2);
+  --it;
+  EXPECT_EQ(*it, 1);
+
+  EXPECT_EQ(it, myList.begin());
+  --it;
+  EXPECT_EQ(it, myList.end());  // 1-2-3-4-5-nulptr
+  --it;
+  EXPECT_EQ(*it, 5);
+}
+TEST(Test_List, Copy_Constructor) {
+  s21::list<int> myList1 = {1, 2, 3, 4, 5};
+  s21::list<int> myList2(myList1);
+
+  EXPECT_EQ(myList2.getSize(), myList1.getSize());
+  EXPECT_NE(myList2.getHead(), myList1.getHead());
+
+  auto it1 = myList1.begin();
+  auto it2 = myList2.begin();
+  EXPECT_EQ(*it2, *it1);
+
+  it1 = myList1.end();
+  it2 = myList2.end();
+  --it1;
+  --it2;
+  EXPECT_EQ(*it2, *it1);
+}
+
+TEST(ListMoveConstructorTest, MoveNonEmptyList) {
+  s21::list<int> original{1, 2, 3};
+  s21::list<int> moved(std::move(original));
+
+  EXPECT_EQ(moved.getSize(), 3);
+  EXPECT_EQ(*moved.begin(), 1);
+  EXPECT_EQ(*++moved.begin(), 2);
+  EXPECT_EQ(*++(++moved.begin()), 3);
+
+  EXPECT_EQ(original.getSize(), 0);
+  EXPECT_TRUE(original.begin() == original.end());
+}
+
+TEST(ListMoveConstructorTest, MoveEmptyList) {
+  s21::list<int> original;
+  s21::list<int> moved(std::move(original));
+
+  EXPECT_EQ(moved.getSize(), 0);
+  EXPECT_TRUE(moved.begin() == moved.end());
+
+  EXPECT_EQ(original.getSize(), 0);
+  EXPECT_TRUE(original.begin() == original.end());
+}
+
+TEST(ListAssignmentOperatorTest, AssignNonEmptyList) {
+  s21::list<int> list1 = {1, 2, 3};
+  s21::list<int> list2;
+  list2 = list1;
+
+  EXPECT_EQ(list2.getSize(), list1.getSize());
+  EXPECT_NE(list2.getHead(), list1.getHead());
+
+  auto it1 = list1.begin();
+  auto it2 = list2.begin();
+  while (it1 != list1.end() && it2 != list2.end()) {
+    EXPECT_EQ(*it1, *it2);
+    ++it1;
+    ++it2;
+  }
+}
+
+TEST(ListAssignmentOperatorTest, AssignEmptyList) {
+  s21::list<int> list1;
+  s21::list<int> list2 = {1, 2, 3};
+  list1 = list2;
+
+  EXPECT_EQ(list1.getSize(), list2.getSize());
+  EXPECT_NE(list1.getHead(), list2.getHead());
+
+  auto it1 = list1.begin();
+  auto it2 = list2.begin();
+  while (it1 != list1.end() && it2 != list2.end()) {
+    EXPECT_EQ(*it1, *it2);
+    ++it1;
+    ++it2;
+  }
+}
+
+TEST(ListAssignmentOperatorTest, SelfAssignment) {
+  s21::list<int> list1 = {1, 2, 3};
+  list1 = list1;
+
+  EXPECT_EQ(list1.getSize(), 3);
+  auto it = list1.begin();
+  EXPECT_EQ(*it, 1);
+  ++it;
+  EXPECT_EQ(*it, 2);
+  ++it;
+  EXPECT_EQ(*it, 3);
+}
+TEST(ListAssignmentOperatorTest, MoveAssignmentOperator) {
+  s21::list<int> list1 = {1, 2, 3};
+  s21::list<int> list2;
+  list2 = std::move(list1);
+
+  EXPECT_EQ(list2.getSize(), 3);
+  EXPECT_EQ(list1.getSize(), 0);
+  EXPECT_TRUE(list1.begin() == list1.end());
+  EXPECT_NE(list2.getHead(), nullptr);
+
+  auto it2 = list2.begin();
+  EXPECT_EQ(*it2, 1);
+  ++it2;
+  EXPECT_EQ(*it2, 2);
+  ++it2;
+  EXPECT_EQ(*it2, 3);
+}
